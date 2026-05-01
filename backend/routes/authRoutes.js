7eb45befabf368067d9e79b74380ea753a7d03f5
jyothika1803 <jyothikarajesh1809@gmail.com>
2026-05-01 21:08:23 +0530

    updated frontend apis

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { signup, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/auth/signup", signup);
+router.post('/auth/login', login);
 
 export default router;
